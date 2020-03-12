0527c82146e1d209a18b8f11dc4b48d3002d2e69
Joel Gomez Sanchez <joelgomezsanchez00@gmail.com>
2020-03-12 11:53:45 -0600

    Update SQL to match mysql dump

--- p3.sql
+++ p3.sql
@@ -1,3 +1,52 @@
-CREATE DATABASE paginas;
-USE paginas;
-CREATE TABLE paginas (url VARCHAR(255) KEY, status BOOLEAN);
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 12, 2020 at 06:52 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `paginas`
+--
+CREATE DATABASE IF NOT EXISTS `paginas` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `paginas`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paginas`
+--
+
+DROP TABLE IF EXISTS `paginas`;
+CREATE TABLE `paginas` (
+  `url` varchar(255) NOT NULL,
+  `status` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `paginas`
+--
+ALTER TABLE `paginas`
+  ADD PRIMARY KEY (`url`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
